8aac234a8e01e44b1162139573456d17eedb587f
hafiyaiman1998 <hafiyyaimann1998@gmail.com>
2024-09-10 18:33:49 +0800

    update js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,20 @@
-import './bootstrap';
-import 'admin-lte';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RemarkComponent from './components/RemarkComponent.jsx';
+
+console.log('app.js is loaded');
+
+window.renderNodeComponent = function(data) {
+  console.log('renderNodeComponent is called with data:', data);
+  const container = document.createElement('div');
+  container.id = 'remarkComponent';
+  document.getElementById('getModal').innerHTML = ''; // Clear previous content
+  document.getElementById('getModal').appendChild(container);
+
+  ReactDOM.render(
+    <RemarkComponent data={data} />,
+    document.getElementById('remarkComponent')
+  );
+
+  $('#uploadModal').modal('show');
+};
