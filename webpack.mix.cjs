c1e9fdffbb5f2255cc04ac7cfac71c6d89275f37
hafiyaiman1998 <hafiyyaimann1998@gmail.com>
2025-02-07 12:25:50 +0800

    Update webpack.mix.cjs

--- webpack.mix.cjs
+++ webpack.mix.cjs
@@ -1,26 +1,8 @@
 const mix = require('laravel-mix');
-const path = require('path');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
-
- mix.js('resources/js/app.js', 'public/js')
- .vue()
- .react() // Enables React support
- .postCss('resources/css/app.css', 'public/css', [])
- .webpackConfig({
-     resolve: {
-         alias: {
-             '@': path.resolve(__dirname, 'resources/js'), // Points "@" to "resources/js"
-         },
-         extensions: ['.js', '.jsx', '.json'], // Include .jsx in resolution
-     },
- });
+mix.js('resources/js/app.js', 'public/js')
+   .react()
+   .postCss('resources/css/app.css', 'public/css', [
+         //
+   ])
+   .setPublicPath('public');
