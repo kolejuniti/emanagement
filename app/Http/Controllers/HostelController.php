4247d9db826354d3e1a059e03c2d34e5c8d066ad
hafiyaiman1998 <hafiyyaimann1998@gmail.com>
2025-02-07 11:15:49 +0800

    Update HostelController.php

--- app/Http/Controllers/HostelController.php
+++ app/Http/Controllers/HostelController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use Illuminate\Support\Facades\Validator;
 
 class HostelController extends Controller
 {
